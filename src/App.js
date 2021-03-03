98700f54db8b9658a616fbc876727bd0b0499790
tarushi98 <tarushipathak@gmail.com>
2021-03-04 00:27:32 +0530

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
+    useLocation ,
   } from "react-router-dom";
-  import Home from './LandingPage/home';
-  import OurStories from './StorySection/Stories';
-  import ContactForm from './ContactForm/contactform';
-  import Team from "./OurTeam/Team";
+import Home from './LandingPage/home';
+import OurStories from './StorySection/Stories';
+import ContactForm from './ContactForm/contactform';
+import Team from "./OurTeam/Team";
 
+
+const ScrollToTop = (props) => {
+  const { children } = props;
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <main>{children}</main>;
+};
   function App()
   {
         return(
             <>
             <Router>
                 <Switch>
-                    <Route exact path="/"><Home/></Route>
-                    <Route exact path="/Stories"><OurStories/></Route>
-                    <Route exact path="/OurTeam"><Team/></Route>
-                    <Route exact path="/ContactUs"><ContactForm/></Route>
+                    <Route exact path="/"><ScrollToTop><Home/></ScrollToTop></Route>
+                    <Route exact path="/Stories"><ScrollToTop><OurStories/></ScrollToTop></Route>
+                    <Route exact path="/OurTeam"><ScrollToTop><Team/></ScrollToTop></Route>
+                    <Route exact path="/ContactUs"><ScrollToTop><ContactForm/></ScrollToTop></Route>
                 </Switch>
 
             </Router>
